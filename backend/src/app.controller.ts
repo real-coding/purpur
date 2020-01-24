88641bf64dc2fa2367eea317d91c9b2c4be60407
Alexey Mikhaylov <alexeym.vn@gmail.com>
2020-01-24 16:03:53 +0300

    add backend i18next

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() request: Request): string {
+    return this.appService.getHello(request);
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import applyI18nmiddleware from './i18n';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  applyI18nmiddleware(app);
   await app.listen(4000);
 }
 bootstrap();
